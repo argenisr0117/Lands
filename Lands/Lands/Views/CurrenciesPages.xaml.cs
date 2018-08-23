be166ba2be1971b0bc837472161ef6c8da84dcec
Argenis Rosario <argenis0117@hotmail.com>
2018-08-23 16:52:00 -0400

    TabbedPage

---
+++ Lands/Lands/Views/CurrenciesPages.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lands.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CurrenciesPages : ContentPage
+	{
+		public CurrenciesPages ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
