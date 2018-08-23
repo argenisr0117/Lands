2e953e765665e1db91a72410d63b161c4f3f503c
Argenis Rosario <argenis0117@hotmail.com>
2018-08-23 14:42:26 -0400

    Land Detail Page 0

--- Lands/Lands/ViewModels/LandViewModel.cs
+++ Lands/Lands/ViewModels/LandViewModel.cs
@@ -1,6 +1,24 @@
-﻿namespace Lands.ViewModels
+﻿
+namespace Lands.ViewModels
 {
+    using Models;
+
     public class LandViewModel
     {
+        #region Properties
+
+        public Land Land
+        { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public LandViewModel(Land land)
+        {
+            this.Land = land;
+        }
+
+        #endregion
     }
 }
